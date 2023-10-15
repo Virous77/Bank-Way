cfb92b2807f77fb434b7f52c5b5be6c753483b1b
Reetesh Kumar <rajreetesh7@gmail.com>
2023-10-15 12:08:11 +0530

    added new configuration for server-worker

--- client/sw.js
+++ client/sw.js
@@ -1,11 +1,36 @@
-var CACHE_NAME = "my-pwa-cache-v1";
-var urlsToCache = ["/", "./index.html", "./src/App.tsx"];
+var CACHE_NAME = "expensify-service-worker-v1";
+var urlsToCache = [
+  "/",
+  "./index.html",
+  "/account",
+  "/transaction",
+  "/landing",
+  "/transfer",
+];
 self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll(urlsToCache);
     })
   );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (
+            key !== "expensify-service-worker-v1" &&
+            key !== "expensify-dynamic-v1"
+          ) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener("fetch", function (event) {
@@ -16,10 +41,15 @@ self.addEventListener("fetch", function (event) {
         return response || fetch(event.request);
       })
       .then((data) => {
-        return caches.open("dynamic").then((cache) => {
+        return caches.open("expensify-dynamic-v1").then((cache) => {
           cache.put(event.request.url, data.clone());
           return data;
         });
+      })
+      .catch((error) => {
+        if (error.message === "Failed to fetch") {
+          console.log("Network connection lost");
+        }
       })
   );
 });
